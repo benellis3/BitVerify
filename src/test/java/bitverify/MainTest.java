bdbb3c495d8b6b2df1629e0e60292406c39f31c3
rjeady <robertj.eady@gmail.com>
2016-01-31 16:17:52 +0000

    Created src directory structure for code and tests, with empty main method.

---
+++ src/test/java/bitverify/MainTest.java
@@ -0,0 +1,14 @@
+package bitverify;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
