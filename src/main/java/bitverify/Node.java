19eb66134323ec503cd8770d5e1f3217022eedb3
mattcoley <mattcoley@optonline.net>
2016-02-09 12:04:18 +0000

    Changes to Node class. Users can now add entries. Some interaction between classes implemented

--- src/main/java/bitverify/Node.java
+++ src/main/java/bitverify/Node.java
@@ -1,9 +1,22 @@
 package bitverify;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Scanner;
 
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
+import com.squareup.otto.ThreadEnforcer;
+
+import bitverify.OttoExample.OttoEvent;
+import bitverify.block.Block;
+import bitverify.entries.Metadata;
 import bitverify.mining.Miner;
+import bitverify.mining.Miner.BlockFoundEvent;
 import bitverify.network.ConnectionManager;
+
 
 public class Node {
 	private String[] mOptions = {"Start mining", "Add entry", "Search entries", "See statistics", "Exit"};
@@ -14,6 +27,10 @@ public class Node {
 	
 	private ConnectionManager mConnectionManager;
 	
+	private Bus mEventBus;
+	
+	public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	
 	public Node(String[] args) {
 		handleArgs(args);
 	}
@@ -22,7 +39,9 @@ public class Node {
 		// If no args, we will just show default prompt to user
 		if (args.length == 0) {
 			mScanner = new Scanner(System.in);
-			setupNetwork();
+			mEventBus = new Bus(ThreadEnforcer.ANY);
+			mEventBus.register(this);
+			//setupNetwork();
 			userCLISetup();
 		}
 		else {
@@ -79,7 +98,13 @@ public class Node {
 		while (mMiner != null) {
 			stopMiner();
 		}
-		mMiner = new Miner();
+		try {
+			mMiner = new Miner(mEventBus);
+		} catch (SQLException e) {
+			// TODO Handle this
+			e.printStackTrace();
+		}
+		
 		Thread miningThread = new Thread(mMiner);
 		miningThread.start();
 		mOptions[mMiningOptionNum] = "Stop mining";
@@ -95,7 +120,35 @@ public class Node {
 	}
 	
 	private void addEntry() {
+		// Get input from user
+		//TODO Input validation
+		System.out.println("Enter file path:");
+		String filePath = mScanner.nextLine();
+		String hash = "12903910lasfa";
 		
+		System.out.println("Enter file download:");
+		String fileDownload = mScanner.nextLine();
+		
+		System.out.println("Enter file name:");
+		String fileName = mScanner.nextLine();
+		
+		System.out.println("Enter file description:");
+		String fileDescription = mScanner.nextLine();
+		
+		System.out.println("Enter file geolocation:");
+		String fileGeo = mScanner.nextLine();
+		
+		System.out.println("Enter tags seperated by commas:");
+		String tagString = mScanner.nextLine();
+		String [] tags = tagString.split(",");
+		for (int i = 0; i < tags.length; i++) {
+			tags[i] = tags[i].trim();
+		}
+		String fileTimeStamp = getCurrentDatetime();
+		
+		// Construct metadata object for file
+		Metadata data = new Metadata(hash, fileDownload, fileName, fileDescription, fileGeo, fileTimeStamp, tags);
+		// TODO broadcast to everyone
 	}
 	
 	private void searchEntries() {
@@ -107,12 +160,37 @@ public class Node {
 	}
 	
 	private void exitProgram() {
-		mMiner.stopMining();
+		if (mMiner != null)
+			mMiner.stopMining();
 	}
 	
 	private void setupNetwork() {
 		System.out.println("Setting up network...");
-		mConnectionManager = new ConnectionManager();
+		try {
+			mConnectionManager = new ConnectionManager(32903);
+		} catch (IOException e) {
+			System.out.println("Error setting up network. Will try again.");
+			setupNetwork();
+		}
 	}
+	
+   
+    @Subscribe
+    public void onBlockFoundEvent(BlockFoundEvent e) {
+    	Block block = e.getBlock();
+    	//send to database
+    	//send to network
+    }
+
+    @Subscribe
+    public void onAnyEvent(Object o) {
+        // do stuff
+    }
+    
+    private String getCurrentDatetime() {
+    	Calendar cal = Calendar.getInstance();
+    	SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+    	return sdf.format(cal.getTime());
+    }
 
 }
