28d2b461effba6f6ff87c665f30d7c4beae7bf90
rjeady <robertj.eady@gmail.com>
2016-02-25 16:07:47 +0000

    Put back block tests that were accidentally deleted

--- src/test/java/bitverify/block/BlockTest.java
+++ src/test/java/bitverify/block/BlockTest.java
@@ -3,45 +3,145 @@ package bitverify.block;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import com.squareup.otto.Bus;
+import com.squareup.otto.ThreadEnforcer;
 import org.junit.Test;
 
 import bitverify.crypto.KeyDecodingException;
 import bitverify.entries.Entry;
 import bitverify.entries.EntryTest;
+import bitverify.mining.Miner;
 
 
 public class BlockTest {
-    
-    /*Block genesis = Block.simpleGenesisBlock();
-    
+    static Bus bus = new Bus(ThreadEnforcer.ANY);
+
     @Test
-    public void createGenesisBlock(){
-        Block genesis = Block.simpleGenesisBlock();
+    public void createGenesisBlock() throws Exception{
+        Block genesis = Block.getGenesisBlock();
         assertTrue(genesis.isVerified());
-        assertEquals(genesis.getNonce(),0);
+        assertTrue(Block.verifyChain(Arrays.asList(genesis), bus));
     }
-    
+
     @Test
-    public void singleEntrySerialize() throws IOException{
-        Entry entry1;
-        entry1 = EntryTest.generateEntry1();
-        
+    public void changedNonceGenesis() throws Exception{
+        Block genesis = Block.getGenesisBlock();
+        genesis.incrementNonce();
+        assertFalse(Block.verifyChain(Arrays.asList(genesis), bus));
+    }
+
+    @Test
+    public void singleEntrySerialize() throws Exception{
+        Block genesis = Block.getGenesisBlock();
+        Entry entry1 = EntryTest.generateEntry1();
+
         List<Entry> entries = Arrays.asList(entry1);
-        int target = 3;
+        int target = Integer.MAX_VALUE;
         int nonce = 0;
-        Block firstBlock = new Block(genesis,target,nonce,entries);;
-        byte[] serialBlock = firstBlock.serialize();
-        
+        Block firstBlock = new Block(genesis,target,nonce,entries);
+        byte[] serialBlock = firstBlock.serializeHeader();
+
         Block deserializedBlock = Block.deserialize(serialBlock);
-        
-        assertTrue(Arrays.equals(firstBlock.getPrevBlockHash(), deserializedBlock.getPrevBlockHash()));
-        assertEquals(firstBlock.getTimeStamp(),deserializedBlock.getTimeStamp());
-        assertTrue(Arrays.equals(firstBlock.hash(), deserializedBlock.hash()));
-        assertEquals(firstBlock.getTarget(),deserializedBlock.getTarget());
-        
-    }*/ //TODO UNCOMMENT
-    
+
+        assertTrue(firstBlock.equals(deserializedBlock));
+        assertFalse(deserializedBlock.isVerified());
+        assertTrue(deserializedBlock.setEntriesList(entries));
+        assertTrue(deserializedBlock.isVerified());
+        assertTrue(Block.verifyChain(Arrays.asList(genesis,firstBlock), bus));
+    }
+
+    @Test
+    public void listTwoValidBlocksVerified() throws Exception{
+        Block genesis = Block.getGenesisBlock();
+        Entry entry1 = EntryTest.generateEntry1();
+        List<Entry> entries1 = Arrays.asList(entry1);
+        int target = Integer.MAX_VALUE;
+        int nonce = 0;
+
+        Block block1 = new Block(genesis,target,nonce,entries1);
+
+        assertTrue(Block.verifyChain(Arrays.asList(genesis,block1), bus));
+
+    }
+
+    @Test
+    public void listTwoBlocksOneNotMeetsTarget() throws Exception{
+        Block genesis = Block.getGenesisBlock();
+        Entry entry1 = EntryTest.generateEntry1();
+        List<Entry> entries1 = Arrays.asList(entry1);
+        int target = 0;
+        int nonce = 0;
+
+        Block block1 = new Block(genesis,target,nonce,entries1);
+
+        assertFalse(Block.verifyChain(Arrays.asList(genesis,block1), bus));
+        assertFalse(block1.setEntriesList(entries1));
+    }
+
+    @Test
+    public void listTwoInvalidBlocks() throws Exception{
+        Block genesis = Block.getGenesisBlock();
+        Entry entry1 = EntryTest.generateEntry1();
+        Entry entry2 = EntryTest.generateEntry2();
+        List<Entry> entries1 = Arrays.asList(entry1);
+        List<Entry> entries2 = Arrays.asList(entry2);
+        int target = Integer.MAX_VALUE;
+        int nonce = 0;
+
+        Block block1 = new Block(genesis,target,nonce,entries1);
+        Block block2 = new Block(block1,target,nonce,entries2);
+
+        assertFalse(Block.verifyChain(Arrays.asList(genesis,block2), bus));
+    }
+
+    @Test
+    public void listTwoInvalidTimeStamps() throws Exception{
+        Block genesis = Block.getGenesisBlock();
+        Entry entry1 = EntryTest.generateEntry1();
+        List<Entry> entries1 = Arrays.asList(entry1);
+        int target = Integer.MAX_VALUE;
+        int nonce = 0;
+
+        Block block1 = new Block(genesis,1455745984017l,target,nonce,entries1);
+
+        assertFalse(Block.verifyChain(Arrays.asList(genesis,block1), bus));
+    }
+
+    @Test
+    public void setOneEntryValid() throws IOException{
+        Block genesis = Block.getGenesisBlock();
+        Entry entry1 = EntryTest.generateEntry1();
+
+        List<Entry> entries = Arrays.asList(entry1);
+        int target = Integer.MAX_VALUE;
+        int nonce = 0;
+        Block firstBlock = new Block(genesis,target,nonce,entries);
+        byte[] serialBlock = firstBlock.serializeHeader();
+        Block deserializedBlock = Block.deserialize(serialBlock);
+        assertTrue(deserializedBlock.setEntriesList(Arrays.asList(entry1)));
+    }
+
+    @Test
+    public void setOneEntryInvalid() throws IOException{
+        Block genesis = Block.getGenesisBlock();
+        Entry entry1 = EntryTest.generateEntry1();
+        Entry entry2 = EntryTest.generateEntry2();
+
+        List<Entry> entries = Arrays.asList(entry1);
+        int target = Integer.MAX_VALUE;
+        int nonce = 0;
+        Block firstBlock = new Block(genesis,target,nonce,entries);
+        byte[] serialBlock = firstBlock.serializeHeader();
+        Block deserializedBlock = Block.deserialize(serialBlock);
+        assertFalse(deserializedBlock.setEntriesList(Arrays.asList(entry2)));
+
+    }
+
+
+
 }
